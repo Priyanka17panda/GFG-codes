21ee9437525f8b91d42922ccad17429b865db8fb
Priyanka Panda <148116130+Priyanka17panda@users.noreply.github.com>
2026-02-04 21:12:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Repeating Element/first-repeating-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int firstRepeated(int[] arr) {
+        // code here
+        int n=arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                // If a repeating element is found, return its index
+                if (arr[i] == arr[j]) {
+                    return i+1;
+                }
+            }
+        }
+        // If no repeating element is found, return -1
+        return -1;
+    }
+}
